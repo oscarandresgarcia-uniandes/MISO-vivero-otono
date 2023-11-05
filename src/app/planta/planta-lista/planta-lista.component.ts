0f86a8fdbd39858c663dc2eaf978b1ca181c9952
Andres Garcia <oa.garcia2@uniandes.edu.co>
2023-11-05 00:51:51 -0500

    Incluir totales para plantas de exterior e interior

--- src/app/planta/planta-lista/planta-lista.component.ts
+++ src/app/planta/planta-lista/planta-lista.component.ts
@@ -9,12 +9,16 @@ import { PlantaService } from '../planta.service';
 })
 export class PlantaListaComponent implements OnInit {
   plantas: Array<Planta> = [];
+  total_exterior:number = 0;
+  total_interior:number = 0;
 
   constructor(private plantaService:PlantaService) { }
 
   getPlantas(): void {
     this.plantaService.getPlantas().subscribe((plantas) => {
       this.plantas = plantas;
+      this.total_exterior = this.plantas.filter(planta => planta.tipo == 'Exterior').length;
+      this.total_interior = this.plantas.filter(planta => planta.tipo == 'Interior').length;
     });
   }
 
