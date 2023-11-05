c4dfcd0a34b45af2ed32a3ef5c1bbdd73480299e
Andres Garcia <oa.garcia2@uniandes.edu.co>
2023-11-04 22:44:11 -0500

    Asociación de modelo y componente

--- src/app/planta/planta-lista/planta-lista.component.ts
+++ src/app/planta/planta-lista/planta-lista.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Planta } from '../planta';
 
 @Component({
   selector: 'app-planta-lista',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./planta-lista.component.css']
 })
 export class PlantaListaComponent implements OnInit {
+  plantas: Array<Planta> = [];
 
   constructor() { }
 
