f0e157852d7b34a44d898cee4a1849d6708cecdb
Andres Garcia <oa.garcia2@uniandes.edu.co>
2023-11-04 22:53:19 -0500

    configuración URL del servicio de plantas

--- src/app/planta/planta.service.ts
+++ src/app/planta/planta.service.ts
@@ -1,11 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+import { Planta } from './planta';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlantaService {
+private apiUrl: string = environment.baseUrl;
 
 constructor(private http:HttpClient) { }
+
+getPlantas(): Observable<Planta[]> {
+  return this.http.get<Planta[]>(this.apiUrl);
+}
 
 }
