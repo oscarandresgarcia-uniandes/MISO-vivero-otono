e993c54c816d0a3963614820aacbe24fe790f91c
Andres Garcia <oa.garcia2@uniandes.edu.co>
2023-11-04 22:37:18 -0500

    Estructura base de módulos y componentes

---
+++ src/app/planta/planta-lista/planta-lista.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-planta-lista',
+  templateUrl: './planta-lista.component.html',
+  styleUrls: ['./planta-lista.component.css']
+})
+export class PlantaListaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/planta/planta.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PlantaListaComponent } from './planta-lista/planta-lista.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  exports: [PlantaListaComponent],
+  declarations: [PlantaListaComponent]
+})
+export class PlantaModule { }
